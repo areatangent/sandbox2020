384e2557661749bfabef862c19bc4e0ece1aa92e
Kirilenko <areatangent@gmail.com>
2020-09-02 19:56:43 +0400

    Создан класс Person

---
+++ src/main/java/ru/ssau/tk/areatangent/sandbox2020/Person.java
@@ -0,0 +1,43 @@
+package ru.ssau.tk.areatangent.sandbox2020;
+
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private double height;
+    private double weight;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
